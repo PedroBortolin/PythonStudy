de1a86613915a9619bbba035bf2b9972554ec9c4
Pedro Bortolin <pedro_bortolin@hotmail.com>
2022-09-05 19:56:52 -0300

    first examples added

---
+++ exemplos.py
@@ -0,0 +1,78 @@
+####################Trabalhando com tuplas 
+
+#tup =4,5,6
+#print(tup)
+
+#nestedTup = (4,5), (7,8), (9,10)
+#print(nestedTup)
+
+#iterationTuple = tuple([4,0,2])
+#print(iterationTuple)
+
+#sequenceTuple = tuple('string')
+#print(sequenceTuple[0])
+
+#appendTuple = tuple(['foo',[1,2],True])
+#appendTuple[1].append(3)
+#print(appendTuple)
+
+#concatenatedTuple = (4, None, 'foo') + (6,0) + ('bar',)
+#print(concatenatedTuple)
+
+#multipliedTuple = (4,None,'foo')*4
+#print(multipliedTuple)
+
+#unpackTuple = (4,5,6)
+#a,b,c = unpackTuple
+#print(a)
+#print(b)
+#print(c)
+
+
+#a,b = 1,2
+#b,a = a,b
+#print(a)
+#print(b)
+
+#sequenceUnpack = [(1,2,3),(4,5,6),(7,8,9)]
+#for a,b,c in sequenceUnpack:
+#    print('a={0}, b={1}, c={2}'.format(a,b,c))
+
+#values = 1,2,3,4,5
+#a,b,*_ = values
+#print(a)
+#print(b)
+#print(*_)
+
+
+#contTuple = (1,2,1,2,2,2,1,2,1,2)
+#print(contTuple.count(2))
+
+##########Trabalhando com listas
+
+#listExample = ['red', 'green', 'blue' , 'purple']
+#listExample[1] = 'cian'
+#listExample.append('yellow')
+#listExample.insert(1, 'fuxia')
+#listExample.pop(2)
+#listExample.remove('blue')
+#print(listExample)
+#print('fuxia' in listExample)
+#print('brown' not in listExample)
+
+
+#concatList = [4,None,'foo'] + [7,8,(9,10)]
+#print(concatList)
+
+#listExtend = [4, None, 'foo']
+#listExtend.extend([1,2,3])
+#print(listExtend)
+
+#sortingList = [5,7,4,3,9,1,6,8,2]
+#sortingList.sort()
+#print(sortingList)
+
+#sortingLists2 = ['saw','hammer','axes','nails']
+#sortingLists2.sort(key=len)
+#print(sortingLists2)
+
